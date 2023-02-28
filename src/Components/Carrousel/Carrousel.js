a708653e65f668af0ca591632118f9deecba0c7e
Dumerat <hermelineyoan@gmail.com>
2023-03-01 00:51:31 +0100

    Patch For single image

--- src/Components/Carrousel/Carrousel.js
+++ src/Components/Carrousel/Carrousel.js
@@ -15,15 +15,20 @@ export default function Carrousel({sliderImage}) {
         if(currentPhoto === 0) { setCurrentPhoto(sliderImage.length - 1) }
     }
 
+    const isSingleImage = sliderImage.length === 1;
 
 return(
     <div className="housing-img-container">
         <img src={sliderImage[currentPhoto]} alt="blabla title" />
-        <div className='housing-arrow-container'>
-            <img onClick={previousPhoto} className='housing-nav-arrow-left' src='/images/left-arrow.svg' alt='Previous'></img>
-            <img onClick={nextPhoto} className='housing-nav-arrow-right' src='/images/left-arrow.svg' alt='Next'></img>
-        </div>
-        <p className="slide-count">{currentPhoto + 1} / {sliderImage.length}</p>
+        {!isSingleImage && (
+                <div className='housing-arrow-container'>
+                    <img onClick={previousPhoto} className='housing-nav-arrow-left' src='/images/left-arrow.svg' alt='Previous'></img>
+                    <img onClick={nextPhoto} className='housing-nav-arrow-right' src='/images/left-arrow.svg' alt='Next'></img>
+                </div>
+            )}
+        {!isSingleImage && (
+                <p className="slide-count">{currentPhoto + 1} / {sliderImage.length}</p>
+            )}
     </div>
     )
 }
